52da455f51af753c94d954604a4d150c2fd6ed4c
Natalia <nataliaziaja@interia.pl>
2023-06-22 21:38:34 +0200

    Person class modification 4

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -1,11 +1,9 @@
 package pl.zajavka.case2;
 
-import java.math.BigDecimal;
+record Person(String name, String surname) {
 
-record Person(String name, Long age, BigDecimal salary) {
-
-    public Boolean doYouHaveABike() {
-        return true;
+    public String whatIsYourJob() {
+        return "Software developer!";
     }
 
     public Boolean doYouLikePizza() {
